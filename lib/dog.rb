0c9a08dd4c2e3ca11a4ddcbf20e1cf922c2f1fbd
lac417 <lac417@gmail.com>
2018-08-10 14:43:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # dog.rb
+class Dog
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+    end
+  
+  def name
+    @name 
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
+
+dog_one = Dog.new('Bone', 'Dalmation')
+puts dog_one.name
+
+dog_two = Dog.new('Chestyr')
+puts dog_two.breed
