53d4c25c9cce42df462b67380c64003aef607e96
josephinem11 <josephine.man0112@yahoo.com>
2018-08-10 15:32:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# dog.rb
+# # dog.rb
+# class Dog
+#   def initalize(name)
+#     @name = name 
+#     @breed = breed
+#   end
+  
+#   # def name
+#   #   @name =
+#   # end
+  
+    
+#   # def breed
+#   #   @breed = 
+#   # end
+  
+# end
+
+
+  
+
+  
+    
+# # dog_one = dog.new('Coco')
+# # puts dog_one = dog.new 
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,10 @@
-# person.rb
+class Person 
+  def initialize (name)
+    @name = name 
+  end
+#   def name
+#     @name = 
+#   end
+#   person_one = Person.new('Mia')
+#   puts person_one.name
+# end
